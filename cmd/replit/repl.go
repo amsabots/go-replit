61f2fff364994c9d8e6728f24af151b36cf3a296
Andrew Mititi <andymwebi@gmail.com>
2025-02-27 12:11:23 +0300

    Add ability to read stdi via scanner

--- cmd/replit/repl.go
+++ cmd/replit/repl.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -9,4 +13,22 @@ func cleanInput(text string) []string{
 	output := strings.ToLower(text)
 	s := strings.Fields(strings.TrimSpace(output))
 	 return s
+}
+
+
+func startRepl(){
+	scanner := bufio.NewScanner(os.Stdin)
+    scannerLoop:
+ for {
+   fmt.Print("Pokedex > ")
+   if scanner.Scan(){
+    user_command := cleanInput(scanner.Text())
+    if len(user_command) == 0 {continue}
+    fmt.Printf("Your command was: %s\n", user_command[0])
+   }
+   err := scanner.Err(); if err != nil{
+    log.Println(fmt.Errorf("scanner error: %w", err))
+    break scannerLoop
+   }
+ }
 }
