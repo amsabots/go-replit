40d96eb1436caefc9dfbf11a520776ae4c08690c
Andrew Mititi <andymwebi@gmail.com>
2025-02-27 13:36:39 +0300

    Added help and exit command

--- cmd/replit/repl.go
+++ cmd/replit/repl.go
@@ -22,6 +22,11 @@ func init(){
     description: "Exit the Pokedex",
     callback: commandExit,
   }
+  clicommandMap["help"] = cliCommand{
+    name: "help",
+    description: "Displays a help message",
+    callback: helpCommand,
+  }
 }
 
 
@@ -39,9 +44,20 @@ func startRepl(){
  for {
    fmt.Print("Pokedex > ")
    if scanner.Scan(){
-    user_command := cleanInput(scanner.Text())
-    if len(user_command) == 0 {continue}
+    input := cleanInput(scanner.Text())
+    if len(input) == 0 {continue}
     
+    commandWord := input[0]
+    callAction, exists := clicommandMap[commandWord]
+    if !exists {
+      fmt.Println("Unknown command")
+      continue
+    }
+    err := callAction.callback()
+    if err != nil {
+      log.Println(fmt.Errorf("call Action failed: %w", err))
+      continue
+    }
    }
    err := scanner.Err(); if err != nil{
     log.Println(fmt.Errorf("scanner error: %w", err))
@@ -53,5 +69,19 @@ func startRepl(){
 func commandExit() error{
   fmt.Println("Closing the Pokedex... Goodbye!")
   os.Exit(0)
+  return nil
+}
+
+func helpCommand() error {
+  helpMessage := `Welcome to the Pokedex!
+Usage:
+
+`
+  for _, cmd := range clicommandMap {
+    command := cmd.name
+    desc := cmd.description
+    helpMessage += fmt.Sprintf("%s: %s\n", command, desc)
+  }
+  fmt.Println(helpMessage)
   return nil
 }
